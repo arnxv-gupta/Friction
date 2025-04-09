ec0f35a3faf9834c233c57c1eb9a93310b6d6edf
Avi Rana <itzavi1718@gmail.com>
2025-04-10 02:50:20 +0530

    fixes

--- frontend/_components/ui/DropDown.js
+++ frontend/_components/ui/DropDown.js
@@ -16,7 +16,7 @@ export default function ServerDropDown({serverID, adminID, channelDialog, catego
     <>
       <ul className="absolute top-full left-0 w-full bg-[#E4E4E4] dark:bg-[#2C2C2C] text-sm z-100">
         <li
-        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4]  mx-1"
+        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4] dark:hover:bg-[#484747]  mx-1"
         onClick={()=>{
           setInviteOpen(true);
         }}
@@ -26,12 +26,12 @@ export default function ServerDropDown({serverID, adminID, channelDialog, catego
         </li>
         {adminID==localStorage.getItem("userID") && <Link
         href={`/settings/${serverID}`}
-        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4]  mx-1"
+        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4] dark:hover:bg-[#484747]  mx-1"
         >
         <span className="flex items-center"><Icons.Categories.ApplicationsSystem className="inline mr-1"/> Server Setting</span>
         </Link>}
         {adminID==localStorage.getItem("userID") && <li
-        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4]  mx-1"
+        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4] dark:hover:bg-[#484747]  mx-1"
         onClick={()=>{
           categoryDialog(true);
         }}
@@ -39,7 +39,7 @@ export default function ServerDropDown({serverID, adminID, channelDialog, catego
         <span className="flex items-center"><Icons.Actions.FolderNew className="inline mr-1"/> Create Category</span>
         </li>}
         {adminID==localStorage.getItem("userID") && <li
-        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4]  mx-1"
+        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4] dark:hover:bg-[#484747]  mx-1"
         onClick={()=>{
           channelDialog(true)
         }}
@@ -47,7 +47,7 @@ export default function ServerDropDown({serverID, adminID, channelDialog, catego
         <span className="flex items-center"><Icons.Actions.DocumentNew className="inline mr-1"/> Create Channel</span>
         </li>}
         {adminID!=localStorage.getItem("userID") && <li
-        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4]  mx-1"
+        className="px-4 py-2 rounded my-2 flex items-center hover:bg-[#E4E4E4] dark:hover:bg-[#484747]  mx-1"
         onClick={()=>{
           fetch(`http://localhost:3030/leaveServer?userID=${localStorage.getItem("userID")}&serverID=${serverID}`).then(res=>res.json()).then(data=>{
             console.log(data);
