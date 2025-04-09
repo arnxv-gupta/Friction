ad3f1922dfda4bfdf327c6f635c1defeffc54790
Avi Rana <itzavi1718@gmail.com>
2025-04-10 02:44:25 +0530

    finished join page

--- frontend/app/join/[serverID]/page.js
+++ frontend/app/join/[serverID]/page.js
@@ -3,11 +3,11 @@ import Image from 'next/image';
 import { useContext, useEffect, useState } from 'react';
 import { socketContext } from '@/app/layout';
 import { useRouter } from 'next/navigation';
+import { AnimatedBackground } from 'animated-backgrounds';
 
 export default function Join({params}) {
     const {sendMessage} = useContext(socketContext);
 
-    const username = localStorage.getItem("userID");
     let router = useRouter()
 
     const [data, setData] = useState(null);
@@ -15,26 +15,18 @@ export default function Join({params}) {
     useEffect(()=>{
         fetch(`http://localhost:3030/serverInfo?serverID=${params.serverID}`).then(res=>res.json()).then(data=>{
             setData(data.res);
+            console.log(data);
+            
         })
     }, [])
 
     return (
         <div
             className="flex h-screen items-center justify-center bg-cover bg-center"
-            style={{
-                backgroundImage: "url('/bg.jpg')",
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundAttachment: 'fixed'
-            }}
         >
+                 <AnimatedBackground animationName="gradientWave"/>
             <div
-                className="relative w-full max-w-md p-10 space-y-6 bg-white bg-opacity-90 text-gray-900 rounded-md shadow-lg"
-                style={{
-                    maxHeight: '600px',
-                    overflow: 'hidden',
-                }}
+                className="relative w-full max-w-md p-10 space-y-6 dark:border-neutral-700 bg-neutral-100 dark:bg-neutral-800 rounded-md"
             >
                 <div className="flex justify-center">
                     <img
@@ -42,14 +34,14 @@ export default function Join({params}) {
                         alt="Profile Image"
                         width={120}
                         height={120}
-                        className="rounded-full border-4 border-white shadow-lg"
+                        className="rounded-lg shadow-lg"
                     />
                 </div>
-                <h2 className="text-2xl font-semibold text-center text-gray-800 mt-4">
-                    {username} has invited you to join their server
+                <h2 className="text-2xl font-semibold text-center mt-4">
+                    You have been invited to join {data?.name}
                 </h2>
                 <button
-                    className="bg-blue-600 w-full py-3 px-4 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-md transition-all duration-200"
+                    className="bg-[#51956d] text-[#fff] w-full px-4 py-2 rounded cursor-pointer hover:underline"
                     onClick={()=>{
                         fetch(`http://localhost:3030/joinServer?serverID=${params.serverID}&userID=${localStorage.getItem("userID")}`).then(res=>res.json()).then(data=>{
                             sendMessage("USER JOINED!")
